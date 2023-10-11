ccb995fd5e16383ed077d7c9cccd9fb2ac94c8e8
javi <javier.bravoz@mail.udp.cl>
2023-10-11 14:05:27 -0300

    aside with low stock items added

---
+++ src/Components/Aside/Aside.jsx
@@ -0,0 +1,30 @@
+// Aside.jsx
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './aside.css'
+
+const Aside = () => {
+    const products = useSelector(state => state.products);
+
+    return (
+        <aside>
+            <h3>low stock products</h3>
+            <ul>
+                {products.map(product => {
+                    let textColor;
+                    if (product.stock <= 5) textColor = 'red';
+                    else if (product.stock > 5 && product.stock <= 20) textColor = 'yellow';
+                    else return null;  // Skip products with stock over 20
+
+                    return (
+                        <li key={product.name} style={{ color: textColor }}>
+                            {product.name}: {product.stock} {product.unityType}
+                        </li>
+                    );
+                })}
+            </ul>
+        </aside>
+    );
+}
+
+export default Aside;
--- src/Components/LandingScreen/Landingscreen.jsx
+++ src/Components/LandingScreen/Landingscreen.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './landing.css';
 import Header from '../Header/Header';
 import Stock from '../Main/Stock';
+import Aside from '../Aside/Aside';
 
 const LandingScreen = () => {
     return (
         <div className="landing-screen">
             <Header />
             <Stock />
+            <Aside />
         </div>
     );
 }
